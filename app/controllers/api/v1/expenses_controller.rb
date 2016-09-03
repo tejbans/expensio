e1ac3a8096087375d1cc25a7585f73b77f41a1e1
tejbans <singh.tejbans@gmail.com>
2016-09-03 14:39:47 -0700

    Added expenses controller

---
+++ app/controllers/api/v1/expenses_controller.rb
@@ -0,0 +1,44 @@
+module Api
+ module V1
+  class ExpensesController < ApplicationController
+    skip_before_filter :verify_authenticity_token
+    respond_to :json
+
+    def index
+      respond_with(Expense.all)
+    end
+
+    def show
+      respond_with(Expense.find(params[:id]))
+    end
+
+    def create
+      @expense = Expense.new(expense_params)
+      if @expense.save
+        respond_to do |format|
+          format.json {render :json => @expense}
+        end
+      end
+    end
+
+  def update
+    @expense = Expense.find(params[:id])
+    if @expense.update(expense_params)
+      respond_to do |format|
+        format.json {render :json => @expense}
+      end
+    end
+  end
+
+  def destroy
+    respond_with  Expense.destroy(params[:id])
+  end
+
+  private
+    def expense_params
+      params.require(:expense).permit(:name, :amount)
+    end
+
+  end
+end
+end
