185eca55edfa63b65c65b8bfc2f054982bc89435
tejbans <singh.tejbans@gmail.com>
2016-09-08 16:39:31 -0700

    Modified expensescontroller to use the expense factory

--- app/assets/javascripts/app/controllers/ExpensesController.js
+++ app/assets/javascripts/app/controllers/ExpensesController.js
@@ -1,13 +1,7 @@
-function ExpensesController() {
+function ExpensesController(Expense) {
   var ctrl = this;
-
-  ctrl.expenses = [{
-      name: 'Lunch',
-      amount: 15
-    },{
-      name: 'Phone Bill',
-      amount: 30
-    }];
+ 
+  ctrl.expenses = Expense.query();
 }
 
 angular
--- app/assets/javascripts/app/models/expense.js
+++ app/assets/javascripts/app/models/expense.js
@@ -1,8 +1,8 @@
 function Expense($resource){
-  var Note = $resource('http://localhost:3000/api/v1/expenses/:id.json', {id: '@id'}, {
+  var Expense = $resource('http://localhost:3000/api/v1/expenses/:id.json', {id: '@id'}, {
     update: {method: 'PUT'}
   });
-  return Note;
+  return Expense;
 }
 
 angular
