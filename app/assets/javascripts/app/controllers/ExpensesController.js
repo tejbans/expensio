8bfc49f24d02f1a6887174eca4a4f1f496be8804
tejbans <singh.tejbans@gmail.com>
2016-09-08 14:08:08 -0700

    Added home and expenses controllers

---
+++ app/assets/javascripts/app/controllers/ExpensesController.js
@@ -0,0 +1,15 @@
+function ExpensesController() {
+  var ctrl = this;
+
+  ctrl.expenses = [{
+      name: 'Lunch',
+      amount: 15
+    },{
+      name: 'Phone Bill',
+      amount: 30
+    }];
+}
+
+angular
+  .module('app')
+  .controller('TasksController', TasksController)
