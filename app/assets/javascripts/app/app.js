fc9fc72537851846d4eb80d10e209d80cf54bfd5
tejbans <singh.tejbans@gmail.com>
2016-09-08 13:54:41 -0700

    Added home and new routes to app.js

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,2 +1,16 @@
 angular
   .module('app', ['ui.router', 'ngResource', 'templates'])
+  .config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/templates/home.html',
+        controller: 'HomeController' as ctrl'
+      })
+      .state('home.new', {
+        url: 'new',
+        templateUrl: 'app/templates/home/new.html',
+        controller: 'ExpensesController as ctrl',
+      })
+    $urlRouterProvider.otherwise('/');
+  });
