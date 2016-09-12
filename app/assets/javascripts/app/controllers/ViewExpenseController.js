1212cca6c743f4d50a8417680523f4ad3633a8ea
tejbans <singh.tejbans@gmail.com>
2016-09-12 13:06:12 -0700

    Added ViewExpenseController

---
+++ app/assets/javascripts/app/controllers/ViewExpenseController.js
@@ -0,0 +1,10 @@
+angular
+  .module('app')
+  .controller('ViewExpenseController', ViewExpenseController)
+
+  function ViewExpenseController(Expense, $stateParams){
+    var ctrl = this;
+
+    ctrl.expense = Expense.get({id: #stateParams.id});
+
+  }
