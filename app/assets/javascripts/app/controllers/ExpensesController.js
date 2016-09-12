3ab83df23bd4e4ab5b0aef519bdad4469fd15fa2
tejbans <singh.tejbans@gmail.com>
2016-09-12 13:57:59 -0700

    Added deletexpense function to controller

--- app/assets/javascripts/app/controllers/ExpensesController.js
+++ app/assets/javascripts/app/controllers/ExpensesController.js
@@ -2,7 +2,13 @@ function ExpensesController(Expense) {
   var ctrl = this;
  
   ctrl.expenses = Expense.query();
-}
+
+  ctrl.deleteExpense = function(expense){
+    expense.$delete(function(){
+      $state.go($state.current, {}, {reload: true});
+    });
+  };
+};
 
 angular
   .module('app')
