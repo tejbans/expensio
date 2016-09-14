2d502f4da1658b16e93e437662dfd6ce3388f88d
tejbans <singh.tejbans@gmail.com>
2016-09-13 22:21:47 -0700

    Added description to expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903212012) do
+ActiveRecord::Schema.define(version: 20160914051653) do
 
   create_table "expenses", force: :cascade do |t|
     t.string   "name"
     t.float    "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
