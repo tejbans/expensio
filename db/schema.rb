0dd764afdffbd04b6ac64f92f032ab0332a88a7c
tejbans <singh.tejbans@gmail.com>
2016-09-15 21:21:38 -0700

    Added Categories table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914051653) do
+ActiveRecord::Schema.define(version: 20160916041844) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "expenses", force: :cascade do |t|
     t.string   "name"
