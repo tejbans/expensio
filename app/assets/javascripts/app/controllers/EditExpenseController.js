ba4882508a45edb9c4cc0d36f213c11679f78fdb
tejbans <singh.tejbans@gmail.com>
2016-09-16 11:46:26 -0700

    Added categories variable and injected Category

--- app/assets/javascripts/app/controllers/EditExpenseController.js
+++ app/assets/javascripts/app/controllers/EditExpenseController.js
@@ -2,9 +2,9 @@ angular
   .module('app')
   .controller('EditExpenseController', EditExpenseController)
 
-  function EditExpenseController(Expense, $location, $stateParams){
+  function EditExpenseController(Expense, Category, $location, $stateParams){
     var ctrl = this;
-
+    ctrl.categories = Category.query();
     ctrl.expense = Expense.get({id: $stateParams.id});
 
     ctrl.editExpense = function(){
