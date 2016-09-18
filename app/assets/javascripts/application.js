bc278c442a9a95fcb84b48bb9e1ce0ab0ab646f9
tejbans <singh.tejbans@gmail.com>
2016-09-17 19:29:07 -0700

    Added dependencies for angular-chart and chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,6 @@
 //= require angular-ui-router
 //= require angular-resource
 //= require angular-rails-templates
+//= require chart.js/dist/Chart.js
+//= require angular-chart.js/dist/angular-chart.js
 //= require_tree .
