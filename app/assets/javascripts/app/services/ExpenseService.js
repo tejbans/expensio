3a1313edf1d797da44d94325b9346fa585f4cf8e
tejbans <singh.tejbans@gmail.com>
2016-09-28 09:53:13 -0700

    Added create,read,update and delete expense methods

--- app/assets/javascripts/app/services/ExpenseService.js
+++ app/assets/javascripts/app/services/ExpenseService.js
@@ -2,6 +2,22 @@ function ExpenseService($http){
   this.getExpenses = function(){
     return $http.get('http://localhost:3000/api/v1/expenses');
   }
+  this.createExpense = function(params){
+    return $http.post('http://localhost:3000/api/v1/expenses', params);
+  }
+
+  this.readExpense = function(id){
+    return $http.get('http://localhost:3000/api/v1/expenses/' +id );
+  }
+
+  this.updateExpense = function(params, id){
+    return $http.put('http://localhost:3000/api/v1/expenses/' +id, params);
+  }
+
+  this.deleteExpense = function(id){
+    return $http.delete('http://localhost:3000/api/v1/expenses/' + id);
+  }
+  
 }
 
 
