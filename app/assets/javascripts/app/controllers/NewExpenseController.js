de0a605ca00764c4d0dd5a26c4e10565fef7493d
tejbans <singh.tejbans@gmail.com>
2016-09-28 11:15:25 -0700

    Modified createExpense method

--- app/assets/javascripts/app/controllers/NewExpenseController.js
+++ app/assets/javascripts/app/controllers/NewExpenseController.js
@@ -12,16 +12,16 @@ angular
    // ctrl.expense = new Expense();
     
     ctrl.addExpense = function(){
-      var params={
-        name: ctrl.name,
-        category_id: ctrl.category_id,
-        amount: ctrl.amount,
-        description: ctrl.description
-      }
+        var params={
+          name: ctrl.name,
+          category_id: ctrl.category_id,
+          amount: ctrl.amount,
+          description: ctrl.description
+        }
 
-      ExpenseService.createExpense(params).then(function(response){
-        console.log(response.data);
-      })
+        ExpenseService.createExpense(params).then(function(response){
+          $location.path('expenses');
+        });
     }
    
   
